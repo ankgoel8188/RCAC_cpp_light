64fdb087b1c8e9d558ca5ff09c1454bf973f910f
ankgoel8188 <ankgoel@umich.edu>
2020-12-13 07:19:02 +0000

    RCAC with higher order filter

--- RCAC_V4.cpp
+++ RCAC_V4.cpp
@@ -1,15 +1,14 @@
 #include "RCAC_V4.hpp"
 #include <iostream>
-#include "Eigen/Core"
-#include "Eigen/Sparse"
 
-// using namespace std;
+using namespace std;
 
-RCAC::RCAC(double P0_val, double N1_val, double lambda_val)
+RCAC::RCAC(double P0_val, double lambda_val, int nf_val, Eigen::MatrixXd filtNu_val)
 {
     P0 = P0_val;
     lambda = lambda_val;
-    N1 = N1_val;
+    nf = nf_val;
+    filtNu = filtNu_val;
 }
 
 void RCAC::init_RCAC()
@@ -18,65 +17,72 @@ void RCAC::init_RCAC()
     int lz = 1;
     P.setIdentity(3 * lu, 3 * lu);
     P = P0 * P;
-    // cout << kk << "\n"
-    //      << P << endl;
     theta.setZero(3 * lu);
 
-    u_k.setZero(lu);
     u_km1.setZero(lu);
 
-    z_k.setZero(lz);
     z_km1.setZero(lz);
 
     Phi_k = Eigen::MatrixXd::Zero(lu, 3 * lu);
-    Phi_km1 = Phi_k;
-    Phi_km2 = Phi_k;
+
     Idty_lz.setIdentity(lz, lz);
+
+    ubar.setZero(nf, 1);
+    Phibar.setZero(nf + 1, 3);
+
+    PhibarBlock.setZero(nf, 3);
+    UbarBlock.setZero(nf-1,1);
 }
 
-void RCAC::buildRegressor(double zk, double zk_int, double zk_diff)
+void RCAC::set_RCAC_data(double zkm1, double ukm1)
 {
-    Phi_k(0, 0) = zk;
-    Phi_k(0, 1) = zk_int;
-    Phi_k(0, 2) = zk_diff;
+    z_km1(0, 0) = zkm1;
+    u_km1(0, 0) = ukm1;
 }
 
-void RCAC::set_performance(double zk)
+void RCAC::buildRegressor(double z, double z_int, double z_diff)
 {
-    z_k(0, 0) = zk;
+    Phi_k(0, 0) = z;
+    Phi_k(0, 1) = z_int;
+    Phi_k(0, 2) = z_diff;
+}
+
+void RCAC::filter_data()
+{
+    UbarBlock = ubar.block(0, 0, nf - 1, 1);
+    ubar.block(1, 0, nf - 1, 1) = UbarBlock;
+    ubar(0, 0) = u_km1(0, 0);
+
+    PhibarBlock = Phibar.block(0, 0, nf, 3);
+    Phibar.block(1, 0, nf, 3) = PhibarBlock;
+    Phibar.block(0, 0, 1, 3) = Phi_k;
+
+    z_filt = z_km1;
+    u_filt = filtNu * ubar;
+    Phi_filt = filtNu * Phibar.block(1, 0, nf, 3);
+    // cout << kk << "\t" << z_filt << "\t" << u_filt << "\t" << Phi_filt << endl;
 }
 
 void RCAC::update_theta()
 {
-    z_filt = z_k;
-    u_filt = N1*u_km2;
-    Phi_filt = N1*Phi_km2;
-
     if (kk > 3)
     {
-        Gamma = lambda * Idty_lz + Phi_filt * P * Phi_filt.transpose() * N1;
+        Gamma = lambda * Idty_lz + Phi_filt * P * Phi_filt.transpose();
         P = P - P * Phi_filt.transpose() * Gamma.inverse() * Phi_filt * P;
         P = P / lambda;
         theta = theta - P * Phi_filt.transpose() * (z_filt + Phi_filt * theta - u_filt);
     }
-    // cout << kk << "\t" << z_filt << "\t" << u_filt << "\t" << Phi_filt << "\t" << theta.transpose() << endl;   
+    // cout << kk << "\t" << z_filt << "\t" << u_filt << "\t" << Phi_filt << "\t" << theta.transpose() << endl;
 }
 
-Eigen::VectorXd RCAC::compute_uk(double zk, double zk_int, double zk_diff)
+Eigen::VectorXd RCAC::compute_uk(double z, double z_int, double z_diff, double u)
 {
-    buildRegressor(zk, zk_int, zk_diff);
-    set_performance(zk);
+    set_RCAC_data(z, u);
+    buildRegressor(z, z_int, z_diff);
+    filter_data();
     update_theta();
     u_k = Phi_k * theta;
-    shift_data();
-    return u_k;
-}
-
-void RCAC::shift_data()
-{
-    u_km2 = u_km1;
-    u_km1 = u_k;
-    Phi_km2 = Phi_km1;
-    Phi_km1 = Phi_k;
+    // shift_data();
     kk = kk + 1;
+    return u_k;
 }
--- RCAC_V4.hpp
+++ RCAC_V4.hpp
@@ -2,7 +2,8 @@
 #define _RCAC_new_HPP_
 
 #include "Eigen/Dense"
-#include "Eigen/Sparse"
+#include "Eigen/Core"
+// #include "Eigen/Sparse"
 // #include <iostream>
 
 
@@ -17,10 +18,11 @@ class RCAC
 {
     double P0;
     double lambda;
-    double N1;
+    int nf;
+    Eigen::MatrixXd filtNu;
 
 public:
-    RCAC( double, double, double);
+    RCAC( double, double, int, Eigen::MatrixXd);
     
     /**
          * Returns RCAC's computed value for the control. Must run oneStep at least once.
@@ -63,22 +65,30 @@ public:
     // void buildRegressor(Eigen::VectorXd &zIn);
 
     void init_RCAC();
-    void buildRegressor(double zk, double zk_int, double zk_diff);
-    void set_performance(double zk);
+    void set_RCAC_data(double , double);
+    void buildRegressor(double zkm1, double zkm1_int, double zkm1_diff);
+    void filter_data();
     void update_theta();
-    Eigen::VectorXd compute_uk(double zk, double zk_int, double zk_diff);
-    void shift_data();
+    Eigen::VectorXd compute_uk(double, double, double, double);
         
 protected:
     //RCAC Working variables
     Eigen::MatrixXd P;
     Eigen::VectorXd theta;
 
-    Eigen::VectorXd u_k, u_km1, u_km2, u_filt;
-    Eigen::VectorXd z_k, z_km1, z_filt;
-    Eigen::MatrixXd Phi_k, Phi_km1, Phi_km2, Phi_filt; 
+    Eigen::VectorXd u_k, u_km1, u_filt;
+    Eigen::VectorXd z_km1, z_filt;
+    Eigen::MatrixXd Phi_k, Phi_filt; 
+    
+    Eigen::MatrixXd ubar, Phibar;
+
+    Eigen::MatrixXd UbarBlock, PhibarBlock;
+
     Eigen::MatrixXd Gamma;
     Eigen::MatrixXd Idty_lz;
+
+    
+
     int kk = 1;
 };
 
