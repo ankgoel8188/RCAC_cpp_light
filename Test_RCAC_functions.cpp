ac81ffdc1e44038837c3db8050ea9902c444a4c4
ankgoel8188 <ankgoel@umich.edu>
2021-01-15 19:27:43 -0500

    Moved old files to OldFiles

--- Test_RCAC_functions.cpp
+++ Test_RCAC_functions.cpp
@@ -2,7 +2,8 @@
 #define _RCAC_HPP_
 
 #include <iostream>
-#include "RCAC_V4.hpp"
+// #include "RCAC_V4.hpp"
+#include "RCAC.h"
 // #include "Eigen/Core"
 using namespace std;
 // using namespace Eigen;
@@ -13,7 +14,10 @@ int main()
     filtNu(0,0) = 0;
     filtNu(0,1) = 1;
 
-    RCAC myRCAC(5.0, 1.0, 2, filtNu);
+    // RCAC myRCAC(5.0, 1.0, 2, filtNu);
+    RCAC myRCAC;
+    myRCAC.set_RCAC_parameters(1,1,2,3);
+
     myRCAC.init_RCAC();
     myRCAC.buildRegressor(0,0,0);
     myRCAC.set_RCAC_data(1,2);
@@ -21,6 +25,6 @@ int main()
     myRCAC.update_theta();
     myRCAC.compute_uk(0,0,0,0);
     // uout = myRCAC.compute_uk(z1, g1, zd1,u(0,0));
-    std::cout << filtNu(0,0);
+    std::cout << filtNu(0,0) << endl;
 }
 #endif
